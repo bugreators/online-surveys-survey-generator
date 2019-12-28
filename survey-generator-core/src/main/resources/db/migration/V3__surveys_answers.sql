7b037751e7b744acb90abfbb1ddd4f40a77c9864
vtodev <viktortodev@gmail.com>
2019-12-28 19:42:52 +0100

    V2 migration and migration v3

--- survey-generator-core/src/main/resources/db/migration/V3__surveys_answers.sql
+++ survey-generator-core/src/main/resources/db/migration/V3__surveys_answers.sql
@@ -1,41 +1,43 @@
-DROP SCHEMA IF  EXISTS surveys;
+DROP SCHEMA IF EXISTS surveys cascade;
 CREATE SCHEMA IF NOT EXISTS surveys;
 
-CREATE TABLE IF NOT EXISTS surveys.recipient(
- id bigserial primary key ,
- email text not null ,
- status boolean
+CREATE TABLE IF NOT EXISTS surveys.recipient
+(
+    id     bigserial primary key,
+    email  text not null,
+    status boolean
 );
 
-CREATE TABLE IF NOT EXISTS surveys.surveys(
-    id bigserial primary key ,
-    status boolean not null ,
-    amount integer not null ,
+CREATE TABLE IF NOT EXISTS surveys.surveys
+(
+    id       bigserial primary key,
+    status   boolean   not null,
+    amount   integer   not null,
     end_time timestamp not null
 );
 
-CREATE TABLE IF NOT EXISTS surveys.surveys_recipients(
-  id bigserial primary key ,
-  recipient_id bigserial references surveys.recipient(id),
-  survey_recipient_id bigserial references surveys.surveys_recipients(id)
-);
-CREATE SCHEMA IF NOT EXISTS survey_answers;
-
-CREATE TABLE IF NOT EXISTS survey_answers.report(
-    id bigserial primary key,
-    survey_id bigserial references surveys.surveys(id)
+CREATE TABLE IF NOT EXISTS surveys.surveys_recipients
+(
+    id                  bigserial primary key,
+    recipient_id        bigserial references surveys.recipient (id),
+    survey_recipient_id bigserial references surveys.surveys_recipients (id)
 );
 
-CREATE TABLE IF NOT EXISTS surveys_answers(
-    id bigserial primary key ,
-    status  boolean not null ,
-    survey_id bigserial references surveys.surveys(id)
+CREATE TABLE IF NOT EXISTS surveys.report
+(
+    id        bigserial primary key,
+    survey_id bigserial references surveys.surveys (id)
 );
 
-CREATE TABLE IF NOT EXISTS surveys_answers.question_answers(
-    id bigserial primary key ,
-    question_id bigserial references surveys.survey_templates.survey_question(id),
-    answer integer
+CREATE TABLE IF NOT EXISTS surveys.surveys_answers
+(
+    id        bigserial primary key,
+    status    boolean not null,
+    survey_id bigserial references surveys.surveys (id)
 );
-
-
+CREATE TABLE IF NOT EXISTS surveys.question_answers
+(
+    id          bigserial primary key,
+    question_id bigserial references survey_templates.survey_question (id),
+    answer      integer
+);
